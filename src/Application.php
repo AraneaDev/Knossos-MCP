81124dad96d58930d8b11023980963f5c3074478
Tim Schipper <info@aranea-development.nl>
2026-09-25 00:55:18 +0200

    chore(main): release 0.17.5

--- src/Application.php
+++ src/Application.php
@@ -18,7 +18,7 @@ use Throwable;
  */
 final class Application
 {
-    public const VERSION = '0.17.4'; // x-release-please-version
+    public const VERSION = '0.17.5'; // x-release-please-version
 
     /** @var resource|null */
     private $errorStream;
